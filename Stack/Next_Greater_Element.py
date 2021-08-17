ed5a086d076f86f262e4ca12f23da5a5cc56dcfa
sharmaastha766 <67647389+sharmaastha766@users.noreply.github.com>
2021-08-17 23:21:07 +0530

    Create Next_Greater_Element.py
    Stack

---
+++ Stack/Next_Greater_Element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextLargerElement(self,arr,n):
+        #code here
+        stack=[]
+        ans=[]
+        for i in range(n-1,-1,-1):
+            
+            #print(i)
+            while(len(stack)!=0 and stack[-1]<=arr[i]):
+                stack.pop()
+            if(len(stack)==0):
+                ans.append(-1)
+            else:
+                ans.append(stack[-1])
+            stack.append(arr[i])
+        return ans[::-1]
